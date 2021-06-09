3a56ff320414510f99cb62dbf3a95d4f6ac4df07
daleinen7 <daleinen@gmail.com>
2021-06-08 22:30:16 -0500

    internal links look and behave correct

--- js/nav.js
+++ js/nav.js
@@ -8,8 +8,8 @@ let menuOpen = false;
 
 // on page load, fill mobile nav with regular nav
 const initMobileNav = ()=> {
-  mobileNav.appendChild(intNav).classList.add('mobile')
-  mobileNav.appendChild(extNav).classList.add('mobile')
+  mobileNav.appendChild(intNav).className = 'internal-links-mobile'
+  mobileNav.appendChild(extNav).className = 'external-links-mobile'
 }
 
 initMobileNav();
