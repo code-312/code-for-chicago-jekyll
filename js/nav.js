09ed6764e6094be0a613b0ab61d3926e2f8f9f06
Doug Leinen <daleinen@gmail.com>
2021-06-15 21:56:10 -0500

    Nav bar (#160)
    * match figma down to 990px
    
    * move styles back to main scss, overwrite bootstrap nonsense
    
    * hamburger icons
    
    * start mobile menu toggling
    
    * style mobile internal link menu items
    
    * tsite nav, donate and menu icon all LOOK right
    
    * add nav specific menu that builds on load
    
    * internal links look and behave correct
    
    * external links line up
    
    * responsive down to 360px
    
    * update nav of other pages
    
    * fix social media links align right
    
    * fix nav links align left
    
    * fix accessability issues and include logo in main nav menu
    
    * remove prevent scrolling class
    
    * clarify open menu in drop down, aria-label 'site'
    
    * revert main nav label back to 'main'
    
    Co-authored-by: Juliette Rapala <jrapala@gmail.com>

---
+++ js/nav.js
@@ -0,0 +1,46 @@
+const menuIcon = document.querySelector('.menu-icon').firstChild;
+const body = document.querySelector('body'); 
+const siteNav = document.querySelector('.internal-links');
+const extNav = document.querySelector('.external-links');
+const intNav = document.querySelectorAll('.hideable-int-link');
+const mobileNav = document.querySelector('.mobile-nav');
+let menuOpen = false;
+
+// on page load, fill mobile nav with regular nav
+const initMobileNav = ()=> {
+  intNav.forEach(nav => {
+    mobileNav.firstChild.appendChild(nav.cloneNode(true)).className = 'internal-links-mobile'
+  })
+  mobileNav.firstChild.appendChild(extNav.cloneNode(true)).className = 'external-links-mobile'
+}
+initMobileNav();
+
+const toggleMenu = ()=> {
+  // toggle menu icon
+  menuIcon.parentNode.ariaLabel = 
+    (menuOpen)
+    ? 'open menu'
+    : 'close menu'
+  
+  menuIcon.src = 
+    (menuOpen) 
+    ? '/images/hamburger-open-menu.svg'
+    : '/images/hamburger-close-menu.svg';
+  
+  menuIcon.alt = 
+    (menuOpen) 
+    ? 'open menu'
+    : 'close menu';
+  
+  // if menu is closed, display menu
+  if(!menuOpen)  {
+    mobileNav.style.display = 'block';
+
+  } else {
+    mobileNav.style.display = 'none';
+  }
+  
+  // toggle state of menu
+  mobileNav.ariaExpanded = !menuOpen;
+  menuOpen = !menuOpen;
+}
